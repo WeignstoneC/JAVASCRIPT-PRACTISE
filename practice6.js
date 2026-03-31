f8971305e6bf5e3c01aefc217a26ebf2652beaff
Weignstone Churchill <weignstonechurchill@gmail.com>
2026-03-31 20:15:49 +0300

    Update practice6.js

---
+++ practice6.js
@@ -0,0 +1,17 @@
+let name = "Alice";
+let age = 30; 
+let isStudent = false; 
+let middleName = undefined; 
+let favoriteColor = null;
+let scores = [10, 20, 30]; 
+let person = { name: "Alice", age: 30 };
+let tags = ["javascript", "practice"]; 
+
+console.log("name:", name);
+console.log("age:", age);
+console.log("isStudent:", isStudent);
+console.log("middleName:", middleName);
+console.log("favoriteColor:", favoriteColor);
+console.log("scores:", scores);
+console.log("person:", person);
+console.log("tags:", tags);
